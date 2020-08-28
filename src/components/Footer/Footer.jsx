3a6daffa7f5d9e3fd205aed9e15d0cf1af0f0ff0
prashantpandey9 <prashantpandey94551@gmail.com>
2020-08-28 18:13:42 +0530

    added footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,41 +1,57 @@
 import React, { Component } from 'react'
-
+import './Footer.scss'
 export class Footer extends Component {
     render() {
         return (
             <div>
-                <footer>
-                    <div className="container" id="contact">
-                        <div className="row">
-                            <div className="col-sm-12">
-                                <ul className="sci">
+                <footer className="footer footer-wrappper">
+                    <div className="container">
+                        <div className="row just">
+                            <div className="col-xl-4 col-lg-5 col-md-4 col-sm-6">
+                                <h5>Take the next step toward your personal and professional goals with us.</h5>
+                                <a href="#"><i class="fa fa-twitter"></i></a>
+                                <a href="#"><i class="fa fa-linkedin"></i></a>
+                                <a href="#"><i class="fa fa-facebook"></i></a>
+                            </div>
+                            <div className="col-xl-2 col-lg-3 col-md-4 col-sm-5">
+                                <h5>Useful Links</h5>
+                                <ul>
                                     <li>
-                                        <a href="">
-                                            <i className="fa fa-facebook"></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="">
-                                            <i className="fa fa-twitter"></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="">
-                                            <i className="fa fa-github"></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="https://www.linkedin.com/in/prashantpandey9/">
-                                            <i className="fa fa-linkedin"></i>
-                                        </a>
+                                        <a href="aboutus">AboutUs</a>
                                     </li>
                                 </ul>
-                                <p className="cpryt">
-                                    &copy; Copyright 2019-2020 | Made with<i className="fa fa-heart"></i>
-                                </p>
+                            </div>
+                            <div className="col-xl-2 col-lg-4 col-md-4 col-sm-6">
+                                <h5>Our Vision</h5>
+                                    <ul>
+                                        <li>
+                                        To keep the child inside everyone alive
+                                        </li>
+                                        <li>
+                                            Our Solutions
+                                        </li>
+                                        <li>
+                                            Our Solutions
+                                        </li>
+                                    </ul>
+                            
+                            </div>
+                            <div className="col-xl-2 col-lg-3 col-md-4 col-sm-6">
+                                <h5>Contact Us</h5>
+                                    <ul>
+                                        <li>
+                                        Phone: 8105070961
+                                        </li>
+                                        <li >
+                                        Email: modi.shekhar@live.com
+                                        </li>
+                                    </ul>
                             </div>
                         </div>
-                    </div>
+                        <center><h4>&copy; Copyright 2020</h4></center>
+                        <br/>   
+                    </div>  
+                    
 	            </footer>
             </div>
         )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import NavBar from '../../components/NavBar/NavBar'
-import {Image, Row, Col, Button, Card }from 'react-bootstrap'
+import Footer from '../../components/Footer/Footer'
+import {Image, Row, Col }from 'react-bootstrap'
 import Cards from '../../components/cards/Cards'
 import './Home.scss'
 import car from './faceshield.png'
@@ -8,9 +9,11 @@ import topic1 from './topic1.png'
 import topic2 from './topic2.png'
 import topic3 from './topic3.png'
 import topic4 from './topic4.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 export class Home extends Component {
     render() {
         return (
+
             <div>
                 <div className="container-fluid con1">
                     <NavBar />
@@ -82,9 +85,30 @@ export class Home extends Component {
                         <div className="col-lg-3 col-md-4 col-sm-6">
                             <img src={topic4} className="imgtopic" alt=""/>
                             <h5 className="subtopic">Gamming</h5>
-                        </div>
-                        
+                        </div> 
                     </div>
+                </div>
+                <br/>
+                <br />
+                <br/>
+                <div class="container">
+                    <div className="row">
+                        <div className="col-lg-6 col-md-6 col-sm-12">
+                            <h1 className="left-heading">Our Mission</h1>
+                            <p classname="para-left">-To keep the child inside everyone alive</p>
+                            <p classname="para-left">-To build a community of potential gamers, singers, dancers, sports persons</p>
+                            
+                        </div>
+                        <div className="col-lg-6 col-md-6 col-sm-12">
+                            <img src={topic1} className="imgtopic" alt=""/>
+                        </div>
+                    </div>    
+                </div>
+                
+                <br /><br />
+                <div className="container-fluid con1">
+                    <Footer />
+                    <FontAwesomeIcon icon={['fab', 'apple']} />
                 </div>
                 
                 
