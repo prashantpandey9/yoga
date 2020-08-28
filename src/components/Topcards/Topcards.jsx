2652f3f46ef7e1e7361d2661b972578183d0fd4b
prashantpandey9 <prashantpandey94551@gmail.com>
2020-08-28 22:15:31 +0530

    added map to coponant

--- src/components/Topcards/Topcards.jsx
+++ src/components/Topcards/Topcards.jsx
@@ -1,36 +1,50 @@
 import React from 'react'
 
-function Topcards() {
-    return (
+const data = [
+    {
+      title: "First Title",
+      text: "First Text",
+     
+    },
+    {
+      title: "second Title",
+      text: "second Text",
+      
+    },
+    {
+      title: "third Title",
+      text: "third Text",
+      
+    }
+  ];
+function App() {
+    return data.map((i, index) => <CardView key={index} {...{ i }} />);
+  }
+
+const CardView = ({
+    title = "Default Title",
+    text = "Default Text",
+  }) => (
+    <div className="col-sm-12 col-md-4 col-lg-4">
+    <div className="card card1 mx-auto">
+        <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{text}</p>
+        </div>
+    </div>
+</div>
+);
+
+  
+
+
+
+const Topcards = () =>{
+    return(
         <div className="container" style={{ padding: '0' }}>
             <div className="row">
-                        <div className="col-sm-12 col-md-4 col">
-                            <div className="card card1 mx-auto">
-                                
-                                <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-sm-12 col-md-4 col">
-                        <div className="card card1 mx-auto"> 
-                                <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-sm-12 col-md-4 col">
-                        <div className="card card1 mx-auto">
-                                
-                                <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                <App />
+            </div>
         </div>
     )
 }
