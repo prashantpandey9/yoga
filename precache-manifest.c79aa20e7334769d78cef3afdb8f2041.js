00efb0ebe1a7005a6133598cb024ee52b7ab289a
prashantpandey9 <prashantpandey94551@gmail.com>
2020-08-30 12:27:01 +0530

    Updates

---
+++ precache-manifest.c79aa20e7334769d78cef3afdb8f2041.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64bab86667fde5577004db5bdee17751",
+    "url": "/aboutus/index.html"
+  },
+  {
+    "revision": "4d8210111565e6954f41",
+    "url": "/aboutus/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "ac66d4bd97b65af350c8",
+    "url": "/aboutus/static/css/main.0ca21a66.chunk.css"
+  },
+  {
+    "revision": "4d8210111565e6954f41",
+    "url": "/aboutus/static/js/2.fb42b36d.chunk.js"
+  },
+  {
+    "revision": "eb94d6017d8f0c633f4acf9346a3ecc8",
+    "url": "/aboutus/static/js/2.fb42b36d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac66d4bd97b65af350c8",
+    "url": "/aboutus/static/js/main.74f4c617.chunk.js"
+  },
+  {
+    "revision": "0cca843da59f6f0933c2",
+    "url": "/aboutus/static/js/runtime-main.f4565783.js"
+  },
+  {
+    "revision": "c36d6c0408a81d6705ad69238db06386",
+    "url": "/aboutus/static/media/dance.c36d6c04.jpg"
+  },
+  {
+    "revision": "d44c289b55051378a6f9f2300c7fa735",
+    "url": "/aboutus/static/media/drawing.d44c289b.jpg"
+  },
+  {
+    "revision": "8e8c40cb40be7a06d1367e9586863d1b",
+    "url": "/aboutus/static/media/faceshield.8e8c40cb.png"
+  },
+  {
+    "revision": "6ebbbb6c468ea0369d92bea60ee79d5d",
+    "url": "/aboutus/static/media/music.6ebbbb6c.jpg"
+  },
+  {
+    "revision": "95540454e8d1752e4d344999217fee9b",
+    "url": "/aboutus/static/media/topic1.95540454.png"
+  },
+  {
+    "revision": "5afd60af41e921c68feda6ef86046aec",
+    "url": "/aboutus/static/media/topic2.5afd60af.png"
+  },
+  {
+    "revision": "952e850ad82c07708b56ac268b9edb1c",
+    "url": "/aboutus/static/media/topic3.952e850a.png"
+  },
+  {
+    "revision": "7e4bfb6edca631d02e3ce5cdc40f76ef",
+    "url": "/aboutus/static/media/topic4.7e4bfb6e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/yoga/precache-manifest.87aae12080e35604d820b1fdfd42cd57.js"
+  "/aboutus/precache-manifest.c79aa20e7334769d78cef3afdb8f2041.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yoga/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aboutus/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
